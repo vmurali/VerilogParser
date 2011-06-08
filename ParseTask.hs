41614323787b1078fc30ba830234b12235aeb369
Muralidaran Vijayaraghavan <vmurali@csail.mit.edu>
2011-06-08 16:12:27 -0400

    Tasks implemented. TODO: separate parser from othr shit cleanly

---
+++ ParseTask.hs
@@ -0,0 +1,29 @@
+module ParseTask(parseTask) where
+
+import Lexer
+import DataTypes
+import Text.Parsec
+
+parseTask = do
+  try (do
+         reserved "always"
+         reservedOp "@"
+         parens $ do{reserved "negedge"; lexeme $ string "CLK"}
+      )
+  reserved "begin"
+  reservedOp "#"
+  lexeme $ char '0'
+  semi
+  tasks <- many parseTaskStmt
+  reserved "end"
+  state <- getState
+  let modIr = ir state
+  putState state{ir = modIr{moduleTasks = tasks}}
+
+parseTaskStmt = do
+  reserved "if"
+  parens $ lexeme $ string "RST_N"
+  reserved "if"
+  expr <- parens $ many (noneOf ")")
+  stmt <- lexeme $ manyTill anyChar (try $ string ");\n")
+  return Task{taskExpr = expr, taskStmt = stmt}
